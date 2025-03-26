5efa7ea436eca0f55b33a3704007a0dbf2e369bd
Erik <eitu@mail.ru>
2025-03-26 17:51:44 +0300

    langs

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Prisijungimo duomenys neatitinka.',
+    'password' => 'Slaptažodis neteisingas.',
+    'throttle' => 'Perdaug bandymų prisijungti. Bandykite po :seconds sec.',
+
+];
