283845a34e7c4394ad4b2a66ad6a13c0339078a9
Erik <eitu@mail.ru>
2025-09-11 16:04:36 +0300

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -393,7 +393,7 @@ $data = $data[0];
 
 
 
-                           $watermark = url('watermarkas.png');
+                           $watermark = public_path('watermarkas.png');
                            $path =  storage_path('/app/public/skelbimai/'. $val->hashName());
 // dd( $path);
                           Image::read($val)
